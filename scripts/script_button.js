94d74e9638be24e7e0a6897cfffd33eadbbae62f
yheguy <yannisheguy40@gmail.com>
2024-07-16 13:34:55 +0200

    ajout pages

---
+++ scripts/script_button.js
@@ -0,0 +1,23 @@
+document.getElementById("button_loisir").onclick = function () {
+    document.getElementById("title_loisir").scrollIntoView({ behavior: "smooth" });
+};
+
+document.getElementById("button_competence").onclick = function () {
+    document.getElementById("title_competence").scrollIntoView({ behavior: "smooth" });
+};
+
+document.getElementById("button_langage").onclick = function () {
+    document.getElementById("title_langage").scrollIntoView({ behavior: "smooth" });
+};
+
+document.getElementById("button_diplome").onclick = function () {
+    document.getElementById("title_diplome").scrollIntoView({ behavior: "smooth" });
+};
+
+document.getElementById("button_xp").onclick = function () {
+    document.getElementById("title_xp").scrollIntoView({ behavior: "smooth" });
+};
+
+document.getElementById("photo_accueil").onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
