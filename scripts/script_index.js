7eda66151f0e4583458ab66899a65b121bbffb78
yheguy <yannisheguy40@gmail.com>
2024-07-25 16:37:21 +0200

    ajout pages

--- scripts/script_index.js
+++ scripts/script_index.js
@@ -1,3 +1,10 @@
+
+document.getElementById("button_trombi").onclick = function () {
+    setTimeout(function() {
+        window.location.href = '../trombinoscope.html';
+    }, 300);
+};
+
 document.getElementById("button_loisir").onclick = function () {
     document.getElementById("title_loisir").scrollIntoView({ behavior: "smooth" });
 };
@@ -6,12 +13,9 @@ document.getElementById("button_competence").onclick = function () {
     document.getElementById("title_competence").scrollIntoView({ behavior: "smooth" });
 };
 
-document.getElementById("button_langage").onclick = function () {
-    document.getElementById("title_langage").scrollIntoView({ behavior: "smooth" });
-};
 
-document.getElementById("button_diplome").onclick = function () {
-    document.getElementById("title_diplome").scrollIntoView({ behavior: "smooth" });
+document.getElementById("button_formation").onclick = function () {
+    document.getElementById("title_formation").scrollIntoView({ behavior: "smooth" });
 };
 
 document.getElementById("button_xp").onclick = function () {
@@ -22,12 +26,11 @@ document.getElementById("photo_accueil").onclick = function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
+document.getElementById("button_langage").onclick = function () {
+    document.getElementById("title_langage").scrollIntoView({ behavior: "smooth" });
+};
+
 document.getElementById("button_softskill").onclick = function () {
     document.getElementById("title_softskill").scrollIntoView({ behavior: "smooth" });
 };
 
-document.getElementById("button_trombi").onclick = function () {
-    setTimeout(function() {
-        window.location.href = '../trombinoscope.html';
-    }, 300);
-};
